40cdaf2aac388c126a6714468083bf0a63cd4418
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-07-17 13:26:28 +0800

    complete video 3

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProductsComponent } from './pages/products/products.component';
@@ -7,11 +8,11 @@ import { ServicesComponent } from './pages/services/services.component';
 import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'products', component: ProductsComponent},
-  {path: 'services', component: ServicesComponent},
-  {path: 'contact', component: ContactComponent},
+  {path: 'home', component: HomeComponent, data: {title: 'Home'}},
+  {path: 'about', component: AboutComponent, data: {title: 'About'}},
+  {path: 'products', component: ProductsComponent, data: {title: 'Products'}},
+  {path: 'services', component: ServicesComponent, data: {title: 'Services'}},
+  {path: 'contact', component: ContactComponent, data: {title: 'Contact'}},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
@@ -10,6 +10,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { ServicesComponent } from './pages/services/services.component';
+import { BasePageComponent } from './partials/base-page/base-page.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ServicesComponent } from './pages/services/services.component';
     AboutComponent,
     ContactComponent,
     ProductsComponent,
-    ServicesComponent
+    ServicesComponent,
+    BasePageComponent
   ],
   imports: [
     BrowserModule,
